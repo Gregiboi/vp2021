46e6e659c5ad17eae207630cff4f2e75b34a98a7
Gregor <gregor@user.noreply.github.com>
2021-09-15 15:33:34 +0300

    3 tund lõppes

--- Veebiprog/page.php
+++ Veebiprog/page.php
@@ -1,5 +1,22 @@
 <?php
 	$author_name = "Greg K.";
+	
+	//kontrollin, kas POST info jõuab kuhugi
+	//var_dump($_POST);
+	//kontollime, kas klikiti submit nuppu
+	$todays_adjective_html = null;
+	$todays_adjective_error = null;
+	$todays_adjective = null;
+	if(isset($_POST["adjective_submit"])){
+		//echo "Klikiti!";
+		//<p>Tänane päev on tuuline.</p>
+		if(!emtpy($_POST["todays_adjective_input"])){
+			$todays_adjective_html = "<p>Tänane päev on" .$_POST["todays_adjective_input"] .".</p>";
+		} else {
+			$today_adjective_error = "Palun sisesta tänase kohta sobiv omadussõna!";
+		}
+	}
+	
 	$full_time_now = date("d.m.Y, H:i:s");
 	$weekday_now = date("N");
 	$day_category = "lihtsalt päev";
@@ -13,7 +30,6 @@
 		$day_category = "puhkepäev";
 	}
 	$weekday_names_et = ["esmaspäev", "teisipäev", "kolmapäev", "neljapäev", "reede", "laupäev", "pühapäev"];
-	
 	if($day_category == "koolipäev") {
 		if($hour_now < 8 or $hour_now >= 23){
 			$hour_category = "uneaeg";
@@ -57,6 +73,16 @@
 	$photo_num = mt_rand(0, $file_count - 1);
 	//<img src="kataloog/fail" alt="Tallinna Ülikool">
 	$photo_html = '<img src="' .$photo_dir .$photo_files[$photo_num] .'" alt="Tallinna Ülikool">'; 
+	
+	$photo_list_html = "\n <ul> \n";
+	for($i = 0;$i < $file_count;$i ++) {
+		$photo_list_html .= "<li>" .$photo_files[$i] ."</li> \n";
+	}
+	$photo_list_html .= "</ul> \n";
+	
+	$photo_select_html = "\n" .<select name="photo_select">' ."\n";
+	for($i = 0;$i < $file_count;$i ++){
+		$photo_select_html .= '<option value=
 ?>
 
 <!DOCTYPE html>
@@ -89,6 +115,23 @@ p.thicker {
 	<img src="02.jpg" alt="random meme" width="600">
 	<p class="thicker" style="font-family:Arial , font-size:50px" style="font-size:50px">o wow dancing skeletons</p>
 	<IMG SRC="test.gif">
+	<hr>
+	<form>
+		<input type="test" placeholder="omadussõna tänase kohta" name="todays_adjective_input" value="<?php echo $todays_adjective; ?>">
+		<input type="submit" name="adjective_submit" value="Saada">
+		<span><?php echo $todays_adjective_error; ?></span>
+	</form>
+	<?php echo $todays_adjective_html; ?>
+	<hr>
+	<hr>
+	<form method = "POST">
+		<?php echo $photo_select_html; ?>
+	</form>
+	<hr>
+	<?php
+		echo $photo_html;
+		echo $photo_html;
+		?>
 	<p>Lehe avamise hetk: <?php echo $weekday_names_et[$weekday_now - 1] .", " .$full_time_now .", " .$day_category .", " .$hour_category; ?>.</p>
 	<?php echo $photo_html; ?>
 </body>
